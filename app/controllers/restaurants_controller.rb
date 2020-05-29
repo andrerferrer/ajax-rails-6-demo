d7b901df3725a26bfd2d527ef1e7bc064a42cf77
Andre Ferrer <andrerferrer@hotmail.com>
2020-05-29 19:40:30 -0300

    implement restaurants#destroy

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,21 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[show destroy]
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
     @review = Review.new
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
